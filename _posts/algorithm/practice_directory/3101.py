b46180cd33f9e2c22c2eab967330e311eba09172
amm0124 <amm0124@naver.com>
2024-04-29 20:46:36 +0900

    posting

--- _posts/algorithm/practice_directory/3101.py
+++ _posts/algorithm/practice_directory/3101.py
@@ -1,37 +1,45 @@
 import sys
 
 n,k=map(int, sys.stdin.readline().split())
-jump=sys.stdin.readline().rstrip()
+jump=sys.stdin.readline().strip()
 index={'U':0 ,'R':1, 'D':2, 'L':3}
-pos_x, pos_y=1,1
+pos_x,pos_y = 1,1
+bundle=[1 for _ in range(2*n)] # 1, 2, 3, ...2n-1까지 시작점의 숫자 저장
+diff=1
 
-bundle=[0 for _ in range(2*n)] # 1, 2, 3, ...2n-1까지 시작점을 
-
-
+for i in range(2,n+1) :
+    bundle[i]=bundle[i-1]+diff
+    diff+=1
+  
+for i in range(n+1, 2*n) :
+    bundle[i]=bundle[i-1]+diff
+    diff-=1
+    
+print(bundle)
 # U R D L
 dx=[-1,0,1,0]
 dy=[0,1,0,-1]
 answer=1
+now=1
 for i in range(k) :
     char=jump[i]
     next_x, next_y=pos_x+dx[index[char]], pos_y+dy[index[char]]
     sum_=next_x+next_y
+    b=sum_-1
+    if b<=n : #1~n번째 묶음
+        if b&1==1 : #홀수번째 묶음 
+            start_x=1
+            start_y=b
+        else : #짝수번째 묶음
+            start_x=b
+            start_y=1
+        answer+= bundle[b] + abs(next_x-start_x)+abs(next_y-start_y)
+    else : #n+1 ~ 2n-1번쨰 묶음
+        pass
     
-    start=1+((sum_-1)*(sum_-2))//2
-    end=((sum_)*(sum_-1))//2
     
-    if sum_%2==0: #짝수, 우상향
-        answer+=((next_y)-1)+(start)
-    else: #홀수, 우하향
-        answer+=(next_x)-1+start
-    
+        
+    pos_x=next_x
+    pos_y=next_y
     print("ans: ",answer)
     
-"""if char=='U' :
-    pass
-elif char=='R':
-    pass
-elif char=='D':
-    pass
-else : #char=='R'
-    pass"""
---
+++ _posts/etc/2024-04-29-decorate_git_blog(1).md
@@ -0,0 +1,6 @@
+---
+title: git blog를 꾸며보자 (1) - 방문자 추가
+date: 2024-4-29 19:00:00 +
+categories: [record, etc]
+tags : compiler
+---
